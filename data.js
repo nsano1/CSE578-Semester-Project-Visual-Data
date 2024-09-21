6bbfda92042237646ce0dbc3d775a17f8a6ca2b1
Naomi Sano <nsano1@asu.edu>
2024-09-20 17:03:32 -0700

    bare set up

--- data.js
+++ data.js
@@ -1,1 +1,15 @@
 // load csv here
+
+
+// This function is called once the HTML page is fully loaded by the browser
+document.addEventListener('DOMContentLoaded', function () {
+     
+    Promise.all([d3.csv('data/temp.csv')])
+         .then(function (values) {
+
+            // map out data here
+
+         });
+
+         
+ });
